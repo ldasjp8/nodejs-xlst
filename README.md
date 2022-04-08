fb54d4c13f0c84712c7394e9acdfed63a38c9efe
nakamura196 <na.kamura.1263@gmail.com>
2022-04-08 12:45:56 +0900

    feat: intiial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Node.js で XLST を実行するサンプルリポジトリ
+
+Sample repository for running XLST in Node.js
+
+## Getting started
+
+```bash
+npm install
+node index.js
+```
---
+++ index.js
@@ -0,0 +1,68 @@
+import {
+  DOMImplementationImpl,
+  DOMParserImpl,
+  XMLSerializerImpl,
+} from "xmldom-ts";
+
+import { install, xsltProcess, getParser } from "xslt-ts";
+
+import fs from "fs";
+
+function xmlParse(xml) {
+  const parser = getParser();
+  return parser.parseFromString(xml, "text/xml");
+}
+
+const xmlString = `
+<?xml version="1.0" encoding="UTF-8"?>
+<TEI xmlns:tei="http://www.tei-c.org/ns/1.0">
+   <teiHeader>
+      <fileDesc>
+         <titleStmt>
+            <title>テスト</title>
+         </titleStmt>
+      </fileDesc>
+   </teiHeader>
+</TEI>
+`;
+
+const xsltString = `
+<?xml version="1.0" encoding="UTF-8"?>
+<xsl:stylesheet
+    xmlns:xsl="http://www.w3.org/1999/XSL/Transform"
+    version="1.0"
+    xmlns:tei="http://www.tei-c.org/ns/1.0">
+
+  <xsl:output method="html" />
+
+  <xsl:template match="/">
+    <html>
+        <body>
+            <xsl:apply-templates/>
+        </body>
+    </html>
+  </xsl:template>
+
+  <xsl:template match="*">
+    <xsl:choose>
+        <xsl:when test="name() = 'titleStmt'">
+            <div>Title: <xsl:value-of select="title"/></div>
+        </xsl:when>
+        <xsl:otherwise>
+            <xsl:apply-templates/>
+        </xsl:otherwise>
+    </xsl:choose>
+  </xsl:template>
+
+</xsl:stylesheet>
+`;
+
+install(
+  new DOMParserImpl(),
+  new XMLSerializerImpl(),
+  new DOMImplementationImpl()
+);
+const output = xsltProcess(xmlParse(xmlString), xmlParse(xsltString));
+
+// 書き込み
+fs.writeFileSync("output.html", output);
